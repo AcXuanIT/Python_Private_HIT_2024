88bd445e5c04229d1068e491c858a8215387a56b
AcXuanIT <xuanphucit7575@gmail.com>
2024-10-27 00:42:07 +0700

    Bai04_Buoi02

---
+++ Buoi02/Bai04.py
@@ -0,0 +1,13 @@
+sl = int(input("So Luong hit 15 :"))
+hit = []
+
+for i in range(sl):
+    hoVaTen = input("Ho va ten :")
+    tuoi = int(input("Tuoi :"))
+    gioiTinh = input("Gioi tinh :")
+    honNhan = input("Tinh Trang Hon Nhan :")
+
+    print("Ho va ten :",hoVaTen)
+    print("Tuoi :",tuoi)
+    print("Gioi tinh :",gioiTinh)
+    print("Tinh Trang Hon Nhan :",honNhan)
